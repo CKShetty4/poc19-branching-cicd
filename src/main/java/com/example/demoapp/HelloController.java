0db39adea916e15a4dc333e722d14561c9ce11fa
CKShetty4 <dev.ckshetty@gmail.com>
2026-05-21 05:47:51 +0000

    Updated application for feature branch demo

--- src/main/java/com/example/demoapp/HelloController.java
+++ src/main/java/com/example/demoapp/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "New Changes in Demo!";
+        return "Welcome to POC19 CI/CD Branching Strategy Demo";
     }
 }
